901b3bd6c1d0968b99630b9b84f79476665e0d9b
server-123 <73692229+server-123@users.noreply.github.com>
2023-09-04 19:09:18 +0900

    Create docker_dli_run.sh

---
+++ docker_dli_run.sh
@@ -0,0 +1,5 @@
+echo "sudo docker run --runtime nvidia -it --rm --network host \
+    --volume ~/nvdli-data:/nvdli-nano/data \
+    --volume /tmp/argus_socket:/tmp/argus_socket \
+    --device /dev/video0 \
+    nvcr.io/nvidia/dli/dli-nano-ai:v2.0.2-r32.7.1kr" >
