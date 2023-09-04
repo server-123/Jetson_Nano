48d48665e34b37b2cbc786983a100bbd5a7257da
server-123 <73692229+server-123@users.noreply.github.com>
2023-09-04 19:14:53 +0900

    Update docker_dli_run.sh

--- docker_dli_run.sh
+++ docker_dli_run.sh
@@ -4,3 +4,6 @@ echo "sudo docker run --runtime nvidia -it --rm --network host \
     --volume /tmp/argus_socket:/tmp/argus_socket \
     --device /dev/video0 \
     nvcr.io/nvidia/dli/dli-nano-ai:v2.0.2-r32.7.1kr" >
+
+#chmod +x docker_dli_run.sh
+#권한 부여
